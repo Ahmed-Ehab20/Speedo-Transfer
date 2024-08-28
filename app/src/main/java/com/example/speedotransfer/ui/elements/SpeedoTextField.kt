534e849c158964ce39c8de55e585e891c2b80aef
k-i-k-a <100940677+k-i-k-a@users.noreply.github.com>
2024-08-28 15:54:07 +0300

    Completed textfield component for signup/login flow

--- app/src/main/java/com/example/speedotransfer/ui/elements/SpeedoTextField.kt
+++ app/src/main/java/com/example/speedotransfer/ui/elements/SpeedoTextField.kt
@@ -1,7 +1,11 @@
 package com.example.speedotransfer.ui.elements
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -9,44 +13,88 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Build
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.speedotransfer.R
+import com.example.speedotransfer.ui.theme.Gray70
+import com.example.speedotransfer.ui.theme.Gray700
 
 
-@Preview
 @Composable
-fun SpeedoTextField() {
-    val text = remember {
-        mutableStateOf("")
-    }
-    Column {
-        Text(
-            text = "Full Name",
-            fontSize = 16.sp,
-            modifier = Modifier.padding(start = 10.dp, bottom = 8.dp)
-        )
-        Spacer(modifier = Modifier.size(10.dp))
+fun SpeedoTextField(
+    title: String, label: String, icon: Int=R.drawable.ic_launcher_background,
+    textValue: String, onTextChange: (String) -> Unit,
+    isPassword: Boolean = false,
+    modifier: Modifier = Modifier
+) {
+    var isShown by remember { mutableStateOf(false) }
+    Column(modifier = modifier) {
+        Text(text = title, fontSize = 16.sp, modifier = Modifier
+            .padding(bottom = 8.dp)
+            .fillMaxWidth(),color= Gray700)
         OutlinedTextField(
-            value = text.value, onValueChange = { it ->
-                text.value = it
+            value = textValue,
+            colors = OutlinedTextFieldDefaults.colors(focusedBorderColor = Gray70, unfocusedBorderColor = Gray70),
+            onValueChange = onTextChange,
+            trailingIcon  = {
+                Box(modifier = modifier.padding(end=16.dp)) {
+                    if (isPassword) {
+                        if (!isShown) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.visibility_off),
+                                contentDescription = title,
+                                modifier = modifier
+                                    .size(24.dp)
+                                    .clickable { isShown = !isShown }
+                                    ,
+                                tint = Gray70
+                            )
+                        } else {
+                            Icon(
+                                painter = painterResource(id = R.drawable.visibility_on),
+                                contentDescription = title,
+                                modifier = modifier
+                                    .size(24.dp)
+                                    .clickable { isShown = !isShown },
+                                tint = Gray70
+                            )
+                        }
+
+                    } else {
+                        Icon(
+                            painter = painterResource(id = icon),
+                            contentDescription = title,
+                            modifier = modifier.size(24.dp),
+                            tint = Gray70
+                        )
+                    }
+                }
             },
-            modifier = Modifier
-                .padding(start = 10.dp),
-            shape = RoundedCornerShape(6.dp),
-            placeholder = {
-                Text(text = "Enter your Full Name")
-            }, trailingIcon = {
-                Icon(imageVector = Icons.Filled.Build, contentDescription = "")
-            }
-
+            placeholder = { Text(text = label, color = Gray70, fontSize = 14.sp) },
+            modifier = modifier.fillMaxWidth(),
+            visualTransformation = if (isPassword && !isShown) {
+                PasswordVisualTransformation()
+            } else VisualTransformation.None,
         )
-
     }
+}
 
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun SpeedoTextFieldPreview() {
+    var textValue by remember { mutableStateOf("") }
+    SpeedoTextField(title = "Title", label = "Label",
+        icon = R.drawable.user, textValue = textValue, isPassword = true, onTextChange = {textValue=it})
 }
