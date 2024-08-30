62bbc9cae1d2b05b40df4d6b654e3efbc6381fef
Amr Ehab <102390696+Ahmed-Ehab20@users.noreply.github.com>
2024-08-30 10:25:19 +0300

    Navbar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,12 @@ android {
 dependencies {
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.compose.material:material:1.6.8")
+    implementation("androidx.compose.ui:ui:1.4.3")
+    implementation("androidx.compose.material:material:1.4.3")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.activity:activity-compose:1.7.2")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
