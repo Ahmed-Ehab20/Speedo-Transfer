723497c60067c7a1defdcd5b0ebb931792d12ed2
Ahmed Ehab <102390696+Ahmed-Ehab20@users.noreply.github.com>
2024-09-11 11:47:42 +0300

    Added Set and get for the token

--- app/src/main/java/com/example/speedotransfer/api/BalanceAPIService.kt
+++ app/src/main/java/com/example/speedotransfer/api/BalanceAPIService.kt
@@ -6,12 +6,22 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object BalanceAPIService {
-    private const val token="eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjQyLCJzdWIiOiJ0ZXN0aW5nQGdtYWlsLmNvbSIsImlhdCI6MTcyNTk5MzkwNiwiZXhwIjoxNzI1OTk3NTA2fQ.gWenAGMhe1rpncb5q14ht2UKwHZBX86NOwxHd1-BH_k"
+    private var token: String = ""
+
+    fun setToken(newToken: String) {
+        token = newToken
+    }
+
+    // Create a client with the interceptor using the stored token
     private val client = OkHttpClient.Builder()
-        .addInterceptor(AuthInterceptor(token)) // Add the interceptor
+        .addInterceptor(AuthInterceptor(token))
         .build()
 
-    private val retrofit= Retrofit.Builder().baseUrl(Constants.BASE_URL).client(client).addConverterFactory(GsonConverterFactory.create()).build()
-    val balanceAPI: BalanceAPICallable = retrofit.create(BalanceAPICallable::class.java)
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(Constants.BASE_URL)
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
+    val balanceAPI: BalanceAPICallable = retrofit.create(BalanceAPICallable::class.java)
 }
